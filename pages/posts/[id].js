45a3e64fb2f390c4fd457db07fb28915d066a508
twozeronine <amdusias209@gmail.com>
2021-03-08 15:47:04 +0900

    [id].js로 동적 라우팅 생성함

---
+++ pages/posts/[id].js
@@ -0,0 +1,33 @@
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+// Which returns an array of possible values for id
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+// Which fetches necessary data for the post with id
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
+}
